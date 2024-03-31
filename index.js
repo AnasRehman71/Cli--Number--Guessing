74e46745dcdae14e26e189a08556f6ac2b80cfa7
AnasRehman71 <161876345+AnasRehman71@users.noreply.github.com>
2024-03-31 17:38:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10",
+    }
+]);
+console.log(answers);
+if (answers.UserGuessedNumber === randomnumber) {
+    console.log("congratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number.");
+}
